e2b102430b6e3c337a019e5304b0b044a2bdcef9
Greg P <gplechaty@gmail.com>
2021-07-31 19:56:06 -0600

    cleaned up logic to handle NULL values in add/remove column

--- apps/core/static/js/vanilla/checkin.js
+++ apps/core/static/js/vanilla/checkin.js
@@ -4,12 +4,18 @@ console.log('check js form')
 function sumColumn(elementList, inputType) {
     let sum = 0;
     for (i=0; i < elementList.length; i++) {
-        if (inputType === 'textContent' && elementList[i].value !== '') {
+        if (inputType === 'textContent' && elementList[i].textContent !== '') {
             sum = parseFloat(sum) + parseFloat(elementList[i].textContent);
         } else if (inputType === 'value'  && elementList[i].value !== '') {
             sum = parseFloat(sum) + parseFloat(elementList[i].value);
         };
-        sum = sum.toFixed(2)
+        if (typeof sum !== "string") {
+            sum = sum.toFixed(2);
+        } else if (sum = '0.00') {
+            sum = 0.00;
+        } else {
+            sum = 'error';
+        }
     };
     return sum;
 }
@@ -29,16 +35,19 @@ function newAmount() {
     for (let row of elementList) {
         let lastAmount = row.querySelector("td[name=lastAmount]").textContent;
         let addOrRemoveSum = row.querySelector("input[name=addOrRemove]").value;
+        (addOrRemoveSum === '' ? addOrRemoveSum = 0 : null)
         sum = parseFloat(lastAmount) + parseFloat(addOrRemoveSum);
+        //console.log('lookiehere:', lastAmount, addOrRemoveSum, sum)
         row.querySelector("td[name=newAmount]").textContent = sum;
     }
     
     let resultRow = document.querySelector("tfoot > tr");
-    console.log(resultRow);
+    //console.log(resultRow);
     let lastAmountFooter = resultRow.querySelector("td[name=previousSum]").textContent;
     let addOrRemoveSumFooter = resultRow.querySelector("td[name=addOrRemoveSum]").textContent;
     let sumFooter = parseFloat(lastAmountFooter) + parseFloat(addOrRemoveSumFooter);
-    console.log('let\'s try this', lastAmountFooter, addOrRemoveSumFooter, sumFooter)
+    sumFooter = sumFooter.toFixed(2);
+    //console.log('let\'s try this', lastAmountFooter, addOrRemoveSumFooter, sumFooter)
     resultRow.querySelector("td[name=newAmountSum]").textContent = sumFooter;
 };
 
